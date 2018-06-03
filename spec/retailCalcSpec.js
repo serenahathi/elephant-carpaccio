1858f3a3a2d038eb89d2aef9e0be41f0ad0a6575
Serena Hathi <hathi.serena@gmail.com>
2018-06-03 23:19:11 +0200

    Add ability to calculate taxes

--- spec/retailCalcSpec.js
+++ spec/retailCalcSpec.js
@@ -1,48 +1,73 @@
-describe('Calculator:', function() {
+describe('Calculator:', function () {
 
-  describe("Base order value", function() {
-    it("calculates the order value without discounts and taxes", function(){
-      var calculator = new Calculator(3,3);
-      expect(calculator.calculateOrderValue(3,3)).toEqual(9);
+  describe("Base order value", function () {
+    it("calculates the order value without discounts and taxes", function () {
+      var calculator = new Calculator(3, 3, "UT");
+      expect(calculator.baseOrderValue(3, 3)).toEqual(9);
     });
   });
 
-  describe("Applies a discount", function() {
-    it("applies a 15% discount if the base order value is greater than or equal to 50k ", function() {
-      var calculator = new Calculator(55000,1);
-      calculator.calculateOrderValue(55000,1);
-      expect(calculator.applyDiscount(56000,1)).toEqual(46750);
+  describe("Applies a discount", function () {
+    it("applies a 15% discount if the base order value is greater than or equal to 50k ", function () {
+      var calculator = new Calculator(55000, 1, "UT");
+      expect(calculator.applyDiscount(56000, 1)).toEqual(46750);
     });
 
-    it("applies a 10% discount if the base order value is greater than or equal to 10k ", function() {
-      var calculator = new Calculator(11000,1);
-      calculator.calculateOrderValue(11000,1);
-      expect(calculator.applyDiscount(11000,1)).toEqual(9900);
+    it("applies a 10% discount if the base order value is greater than or equal to 10k ", function () {
+      var calculator = new Calculator(11000, 1, "UT");
+      expect(calculator.applyDiscount(11000, 1)).toEqual(9900);
     });
 
-    it("applies a 7% discount if the base order value is greater than or equal to 7k ", function() {
-      var calculator = new Calculator(8000,1);
-      calculator.calculateOrderValue(8000,1);
-      expect(calculator.applyDiscount(8000,1)).toEqual(7440);
+    it("applies a 7% discount if the base order value is greater than or equal to 7k ", function () {
+      var calculator = new Calculator(8000, 1, "UT");
+      expect(calculator.applyDiscount(8000, 1)).toEqual(7440);
     });
 
-    it("applies a 5% discount if the base order value is greater than or equal to 5k ", function() {
-      var calculator = new Calculator(6000,1);
-      calculator.calculateOrderValue(6000,1);
-      expect(calculator.applyDiscount(6000,1)).toEqual(5700);
+    it("applies a 5% discount if the base order value is greater than or equal to 5k ", function () {
+      var calculator = new Calculator(6000, 1, "UT");
+      expect(calculator.applyDiscount(6000, 1)).toEqual(5700);
     });
 
-    it("applies a 3% discount if the base order value is greater than or equal to 1k ", function() {
-      var calculator = new Calculator(1100,1);
-      calculator.calculateOrderValue(1100,1);
-      expect(calculator.applyDiscount(1100,1)).toEqual(1067);
+    it("applies a 3% discount if the base order value is greater than or equal to 1k ", function () {
+      var calculator = new Calculator(1100, 1, "UT");
+      expect(calculator.applyDiscount(1100, 1)).toEqual(1067);
     });
 
-    it("does not apply a discount if the base order value is less than 1k", function() {
-      var calculator = new Calculator(50,2);
-      calculator.calculateOrderValue(50,2)
-      expect(calculator.applyDiscount(50,2)).toEqual(100);
+    it("does not apply a discount if the base order value is less than 1k", function () {
+      var calculator = new Calculator(50, 2, "UT");
+      expect(calculator.applyDiscount(50, 2)).toEqual(100);
     })
   })
-});
 
+  describe("Applies taxes", function () {
+
+    beforeEach(function () {
+      var calculator = new Calculator(100, 1, "UT");
+    });
+
+    it("applies a 6.85% tax if UT is entered as a state ", function () {
+      var calculator = new Calculator(100, 1, "UT");
+      expect(calculator.applyTaxes(100, 1, "UT")).toEqual(106.85);
+    });
+
+    it("applies a 8% tax if NV is entered as a state ", function () {
+      var calculator = new Calculator(100, 1, "NV");
+      expect(calculator.applyTaxes(100, 1, "NV")).toEqual(108);
+    });
+
+    it("applies a 6.25% tax if TX is entered as a state ", function () {
+      var calculator = new Calculator(100, 1, "TX");
+      expect(calculator.applyTaxes(100, 1, "TX")).toEqual(106.25);
+    });
+
+    it("applies a 4% tax if AL is entered as a state ", function () {
+      var calculator = new Calculator(100, 1, "AL");
+      expect(calculator.applyTaxes(100, 1, "AL")).toEqual(104);
+    });
+
+    it("applies a 8.25% tax if CA is entered as a state ", function () {
+      var calculator = new Calculator(100, 1, "CA");
+      expect(calculator.applyTaxes(100, 1, "CA")).toEqual(108.25);
+    });
+  });
+});
--- src/retailCalc.js
+++ src/retailCalc.js
@@ -1,24 +1,41 @@
 function Calculator(numberOfItems, pricePerItem) {
-  this.orderValue = 1;
+  this.orderValue = numberOfItems * pricePerItem;
 }
 
-Calculator.prototype.calculateOrderValue = function(numberOfItems, pricePerItem) {
-  return this.orderValue = numberOfItems * pricePerItem;
+Calculator.prototype.baseOrderValue = function (numberOfItems, pricePerItem, state) {
+  return this.orderValue
 };
 
-Calculator.prototype.applyDiscount = function(numberOfItems, pricePerItem) {
+Calculator.prototype.applyDiscount = function (numberOfItems, pricePerItem) {
   if (this.orderValue >= 50000) {
-    this.orderValue = parseInt(this.orderValue * 0.85);
+    this.orderValue *= 0.85;
   } else if (this.orderValue >= 10000) {
-    this.orderValue = (this.orderValue * 0.90);
+    this.orderValue *= 0.90;
   } else if (this.orderValue >= 7000) {
-    this.orderValue = this.orderValue * 0.93;
+    this.orderValue *= 0.93;
   } else if (this.orderValue >= 5000) {
-    this.orderValue = this.orderValue * 0.95;
+    this.orderValue *= 0.95;
   } else if (this.orderValue >= 1000) {
-    this.orderValue = this.orderValue * 0.97;
+    this.orderValue *= 0.97;
   } else {
-    this.orderValue = this.orderValue
+    this.orderValue
   }
-  return this.orderValue.toFixed(2);
+  return this.orderValue;
+};
+
+Calculator.prototype.applyTaxes = function (numberOfItems, pricePerItem, state) {
+  if (state === "UT") {
+    this.orderValue += this.orderValue * 0.0685
+  } else if (state === "NV") {
+    this.orderValue += this.orderValue * 0.08;
+  } else if (state === "TX") {
+    this.orderValue += this.orderValue * 0.0625;
+  } else if (state === "AL") {
+    this.orderValue += this.orderValue * 0.04;
+  } else if (state === "CA") {
+    this.orderValue += this.orderValue * 0.0825;
+  } else {
+    this.orderValue;
+  }
+  return this.orderValue;
 };
