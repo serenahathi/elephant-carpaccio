96bc2971554c28899ef137d2def6cf7fa27c37ed
Serena Hathi <hathi.serena@gmail.com>
2018-06-04 20:31:58 +0200

    Refactor: extract discount logic

--- src/retailCalc.js
+++ src/retailCalc.js
@@ -8,20 +8,24 @@ Calculator.prototype.printOrderValue = function (numberOfItems, pricePerItem) {
 
 Calculator.prototype.applyDiscount = function (numberOfItems, pricePerItem) {
   if (this.orderValue >= 50000) {
-    this.orderValue *= 0.85;
+    this.discounter(85)
   } else if (this.orderValue >= 10000) {
-    this.orderValue *= 0.90;
+    this.discounter(90)
   } else if (this.orderValue >= 7000) {
-    this.orderValue *= 0.93;
+    this.discounter(93)
   } else if (this.orderValue >= 5000) {
-    this.orderValue *= 0.95;
+    this.discounter(95)
   } else if (this.orderValue >= 1000) {
-    this.orderValue *= 0.97;
+    this.discounter(97)
   } else {
     this.orderValue
   }
   return this.orderValue;
 };
+
+Calculator.prototype.discounter = function(discount) {
+  this.orderValue = (this.orderValue / 100) * discount;
+}
 
 Calculator.prototype.applyTaxes = function (state) {
   if (state === "UT") {
